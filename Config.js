00f030bf2915e19ae5efa1e39cd50451659235d4
silayoX340-hub <braysonsilayo340@gmail.com>
2026-05-29 11:19:55 +0300

    Create Config.js

---
+++ Config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    // Bot owner number (without '+' sign)
+    ownerNumber: "25568192847",
+    
+    // Command prefix
+    prefix: ".",
+    
+    // Bot name
+    botName: "SILAYOX V6",
+    
+    // Owner name
+    ownerName: "SILENT DJ",
+    
+    // Session folder
+    sessionFolder: "./session",
+    
+    // Timezone
+    timezone: "Africa/Dar_es_Salaam"
+};
